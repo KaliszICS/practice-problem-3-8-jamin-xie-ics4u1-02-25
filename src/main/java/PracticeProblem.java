07a4c737631b5e5805d3c6ee4e477d6a921e8b83
jamin-xie-ics4u1-02-25 <Jamin.xie@student.tdsb.on.ca>
2025-05-06 02:55:20 +0000

    Update Change

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,8 +1,70 @@
+import java.util.HashMap;
+
 public class PracticeProblem {
 
 	public static void main(String args[]) {
 
 	}
 
-	
+	public static int fib(int num) {
+		HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+		
+		return fibHelper(map, num);
+	}
+
+	public static int fibHelper(HashMap<Integer, Integer> map, int num) {
+		if (num == 0) {
+			return 0;
+		}
+		if (num == 1) {
+			return 1;
+		}
+		if (map.containsKey(num)) {
+			return map.get(num);
+		}
+
+		int value = fibHelper(map, num - 1) + fibHelper(map, num - 2);
+		map.put(num, value);
+		return value;
+	}
+
+	public static int minCostClimbingStairs(int[] cost) {
+		int payed = 0;
+		int index = 0;
+
+		HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+		int upOne = stairHelper(cost, payed, index, map);
+		if (cost.length > 1) {
+			index = 1;
+			int upTwo = stairHelper(cost, payed, index, map);
+			upOne = Math.min(upOne, upTwo);
+		}
+
+		return upOne;
+	}
+
+	public static int stairHelper(int[] cost, int payed, int index, HashMap<Integer, Integer> map) {
+		if (index >= cost.length) {
+			return payed;
+		}
+
+		if (map.containsKey(index)) {
+			return payed + map.get(index);
+		}
+
+		int prevPaid = payed;
+		payed = payed + cost[index];
+
+		int upOne = stairHelper(cost, payed, index + 1, map);
+		int upTwo = stairHelper(cost, payed, index + 2, map);
+
+		if (upOne > upTwo) {
+			payed = upTwo;
+		} else {
+			payed = upOne;
+		}
+		map.put(index, payed - prevPaid);
+		
+		return payed;
+	}
 }
